a8b166e0ac473648729f22edb6c7547673e2b34f
CristhianBravo865 <cristhian.bravo8657@gmail.com>
2025-05-24 17:13:27 -0500

    Se crea el modelo Alumno con campos nombre y codigo

--- alumnos/models.py
+++ alumnos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=100)
+    codigo = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.nombre
