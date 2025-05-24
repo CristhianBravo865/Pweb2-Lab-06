5bd96e73efe7034a5778853779617cf86d063250
CristhianBravo865 <cristhian.bravo8657@gmail.com>
2025-05-24 17:17:16 -0500

    Se crea el modelo NotaAlumnoPorCurso con relaciones y campo nota

--- alumnos/models.py
+++ alumnos/models.py
@@ -13,3 +13,11 @@ class Curso(models.Model):
 
     def __str__(self):
         return self.nombre
+    
+class NotaAlumnoPorCurso(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nota = models.FloatField()
+
+    def __str__(self):
+        return f"{self.alumno} - {self.curso} - {self.nota}"
