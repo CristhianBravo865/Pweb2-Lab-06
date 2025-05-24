0fe9f3a5ae04326f706ea5388fb21528e3b138f2
CristhianBravo865 <cristhian.bravo8657@gmail.com>
2025-05-24 17:15:38 -0500

    Se agrega el modelo Curso con nombre y codigo

--- alumnos/models.py
+++ alumnos/models.py
@@ -6,3 +6,10 @@ class Alumno(models.Model):
 
     def __str__(self):
         return self.nombre
+    
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+    codigo = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.nombre
